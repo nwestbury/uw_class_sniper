d70008cdc5e3f781e816d88332aded7168b4d56e
Nico <5westbury5@gmail.com>
2017-04-23 05:41:55 -0400

    fixed driver, still has errors properly recognizing some buttons but works when it reaches the timeout

--- driver.py
+++ driver.py
@@ -5,33 +5,51 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 
+from time import sleep
+
+class ElementCondition():
+	presence = EC.presence_of_element_located
+	clickable = EC.element_to_be_clickable
+
 class Driver():
-	def __init__(self, username, password, login_url="https://quest.pecs.uwaterloo.ca/psp/SS/ACADEMIC/"):
+	def __init__(self, username, password):
 		"""
 		Initalize login for driver. *Warning* your password is going to be held in plaintext in memory
+		and in the commandline
 		"""
 		self.__username = username
 		self.__password = password
-		self.__login_url = login_url
+		self.__login_url = "https://quest.pecs.uwaterloo.ca/psp/SS/ACADEMIC/"
+		self.__add_url = "https://quest.pecs.uwaterloo.ca/psc/SS/ACADEMIC/SA/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL"
 		self.__driver = webdriver.Chrome()
+		self.__wait_time = 60*60*2
 
 	def start(self):
 		self.__login()
-		self.__go_to_enroll_page()
-		self.__select_tab('add')
+		self.__go_to_add_page()
+		self.__select_latest_term()
+
+		attempt_num = 0
+		while True:
+			self.__attempt_add()
+			self.__restart_add()
+			attempt_num += 1
+			print("Done attempt", attempt_num)
+			sleep(self.__wait_time)
 
 	def __insert_in_input(self, input_field, text):
 		input_field.clear()
 		input_field.send_keys(text)
 
-	def __wait_for_xpath(self, xpath, timeout=10):
+	def __wait_for_xpath(self, xpath, timeout=10, condition=ElementCondition.presence):
 		timeout = 10 # seconds to wait
 		try:
 			# Wait until a particular name input is loaded
-			element_presence = EC.presence_of_element_located((By.XPATH, xpath))
+			element_presence = condition((By.XPATH, xpath))
 			WebDriverWait(self.__driver, timeout).until(element_presence)
 		except TimeoutException:
-			raise TimeoutException("Failed to find {} within {} seconds of page load".format(xpath, timeout))
+			print("Failed to find {} within {} seconds of page load".format(xpath, timeout))
+			raise
 
 	def __login(self):
 		self.__driver.get(self.__login_url)
@@ -49,19 +67,14 @@ class Driver():
 		self.__insert_in_input(password_input, self.__password)
 		submit_button.click()
 
-	def __go_to_enroll_page(self):
-		try:
-			WebDriverWait(self.__driver, 5)
-		except TimeoutException:
-			print("WaitED")
-		
-		self.__driver.excute_script("submitAction_win0(document.win0,'DERIVED_SSS_SCR_SSS_LINK_ANCHOR3');")
+	def __switch_to_internal_iframe(self):
+		iframe_xpath = "//iframe[@id='ptifrmtgtframe']"
+		self.__wait_for_xpath(iframe_xpath)
+		frame = self.__driver.find_element_by_id("ptifrmtgtframe")
+		self.__driver.switch_to.frame(frame)
 
-	def __select_tab(self, name):
-		xpath = "//a[@class=SSSTABMAINTEXT and text()[normalize-space(.)='{}']".format(name)
-		self.__wait_or_xpath(xpath)
-		tab = self.__driver.find_element_by_xpath(xpath)
-		tab.click()
+	def __go_to_add_page(self):
+		self.__driver.get(self.__add_url)
 
 	def __select_latest_term(self):
 		# second option in term select, I assume there will only be two options and the second will be the latest
@@ -69,11 +82,32 @@ class Driver():
 		self.__wait_for_xpath("//a[@id='DERIVED_SSS_SCT_SSR_PB_GO']")
 
 		radio_button = self.__driver.find_element_by_id("SSR_DUMMY_RECV1$sels$1$$0")
-		submit_button = self.__driver.find_element_by_id("DERIVED_SSS_SCT_SSR_PB_GO")
 		radio_button.click()
-		submit_button.click()
+
+		self.__driver.execute_script("submitAction_win0(document.win0,'DERIVED_SSS_SCT_SSR_PB_GO');")
+
+	def __attempt_add(self):
+		try:
+			self.__wait_for_xpath("//a[@id='DERIVED_REGFRM1_LINK_ADD_ENRL$82$'", timeout=2)
+		except TimeoutException:
+			pass
 		
-		
+		self.__driver.execute_script("submitAction_win0(document.win0,'DERIVED_REGFRM1_LINK_ADD_ENRL$82$');")
+
+		try:
+			self.__wait_for_xpath("//a[@id='DERIVED_REGFRM1_SSR_PB_SUBMIT'", timeout=2)
+		except TimeoutException:
+			pass
+
+		self.__driver.execute_script("submitAction_win0(document.win0,'DERIVED_REGFRM1_SSR_PB_SUBMIT');")
+
+	def __restart_add(self):
+		try:
+			self.__wait_for_xpath("//a[@id='DERIVED_REGFRM1_SSR_LINK_STARTOVER'", timeout=2)
+		except TimeoutException:
+			pass
+
+		self.__driver.execute_script("submitAction_win0(document.win0,'DERIVED_REGFRM1_SSR_LINK_STARTOVER');")
 		
 	def exit(self):
 		self.__driver.close()
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from driver import Driver
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Snipe some classes.')
 
-	parser.add_argument('username', type=str, help='an integer for the accumulator')	
-	parser.add_argument('password', type=str, help='an integer for the accumulator')
+	parser.add_argument('username', type=str, help='Quest username')	
+	parser.add_argument('password', type=str, help='Quest password')
 
 	args = parser.parse_args()
 
